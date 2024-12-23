fa9bcf9541512c92b4a2ab430c4dbb1fbc444265
TOMMY AZWAR MUSLIM <85835824+TommyFlutter@users.noreply.github.com>
2024-12-23 21:27:32 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
-  DATABASE_NAME: 'bunda-resto-database',
+  DATABASE_NAME: 'science-resto-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
 
